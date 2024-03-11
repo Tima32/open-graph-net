f0dc107b2452f29936fb1c84ecf86509c6024f85
Tima32 <4timonomit4@gmail.com>
2024-03-12 02:42:18 +0300

    add

--- open-graph-net-service.lua
+++ open-graph-net-service.lua
@@ -1,7 +1,10 @@
 local event = require("event")
+local component = require("component")
+
 
 local function event_handler(target, source, port, distance, message_0)
     print(message_0)
+    component.beep(300)
 end
 
 function start()
@@ -11,4 +14,5 @@ end
 
 function stop()
     print('stop')
+    event.ignore('modem_message', event_handler)
 end
