a15bc6010d3034512548de942354fed6f14851eb
Tima32 <4timonomit4@gmail.com>
2024-03-12 03:03:53 +0300

    fix

--- open-graph-net-service.lua
+++ open-graph-net-service.lua
@@ -2,7 +2,7 @@ local event = require("event")
 local component = require("component")
 
 
-local function event_handler(target, source, port, distance, message_0)
+local function event_handler(event_name, target, source, port, distance, message_0)
     print(message_0)
     component.computer.beep(300)
 end
