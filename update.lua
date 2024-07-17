fabedcb0b649b1c9a887a14db74857ee20a9b469
Tima32 <4timonomit4@gmail.com>
2024-07-18 01:43:35 +0300

    add test to update

--- update.lua
+++ update.lua
@@ -5,5 +5,7 @@ local shell = require('shell')
 local result, reason = fs.makeDirectory('/home/ogn')
 shell.execute('wget -f https://raw.githubusercontent.com/Tima32/open-graph-net/main/update.lua '..
     '/home/ogn/update.lua')
+shell.execute('wget -f https://raw.githubusercontent.com/Tima32/open-graph-net/main/test.lua '..
+    '/home/ogn/test.lua')
 shell.execute('wget -f https://raw.githubusercontent.com/Tima32/open-graph-net/main/open-graph-net-service.lua '..
     '/etc/rc.d/ogn.lua')
