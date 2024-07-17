2b3dd9ad363ef36d0325375c42564d0c9b8e69b3
Tima32 <4timonomit4@gmail.com>
2024-07-18 01:42:19 +0300

    add test.lua

---
+++ test.lua
@@ -0,0 +1,12 @@
+local event         = require("event")
+local component     = require("component")
+local serialization = require("serialization")
+
+local config_port = 123
+
+local modem = component.modem
+
+
+local message = {command='add_me', hostname='test', host_uuid='044b80dc-1234-1234-1234-123456789123'}
+
+modem.broadcast(config_port, serialization.unserialize(message))
