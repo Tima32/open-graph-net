d75f2aad75c3aa729f147a0cf8e20539f0ad53af
Tima32 <4timonomit4@gmail.com>
2024-07-17 20:11:33 +0300

    update update scrypt

--- update.lua
+++ update.lua
@@ -1,4 +1,9 @@
+local fs = require('filesystem')
+
 local shell = require('shell')
 
+local result, reason = fs.makeDirectory('/home/ogn')
+shell.execute('wget -f https://raw.githubusercontent.com/Tima32/open-graph-net/main/update.lua '..
+    '/home/ogn/update.lua')
 shell.execute('wget -f https://raw.githubusercontent.com/Tima32/open-graph-net/main/open-graph-net-service.lua '..
-    '/etc/rc.d/open-graph-net-service.lua')
+    '/home/ogn/open-graph-net-service.lua')
