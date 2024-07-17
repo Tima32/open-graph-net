4b5edf951ac6b86690bc7fbf06922208ce82aa81
Tima32 <4timonomit4@gmail.com>
2024-07-18 01:56:07 +0300

    fix test

--- test.lua
+++ test.lua
@@ -9,4 +9,4 @@ local modem = component.modem
 
 local message = {command='add_me', hostname='test', host_uuid='044b80dc-1234-1234-1234-123456789123'}
 
-modem.broadcast(config_port, serialization.unserialize(message))
+modem.broadcast(config_port, serialization.serialize(message))
